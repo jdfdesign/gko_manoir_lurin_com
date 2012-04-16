dc3183a1c8cda1ab5956b7b76ad916ddff1cc62c
jdfdesign <regis@joufdesign.com>
2012-04-16 15:00:19 -0400

    bump

--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -3,41 +3,72 @@
 //= require jquery.jscrollpane.min.js
 
 $(document).ready(function() { 
+	/////////////////////////////////////////////////
+	var scrollpaneTag = $('.page_content:first'),
+		scrollpaneActive = false,
+		scrollPaneTab,
+		scrollpaneApi,
+		scrollpaneWrapper;
+	
+	/////////////////////////////////////////////////
+	remove_scrollpane = function() {
+		scrollpaneApi.destroy();
+		scrollPaneTab.remove();
+		scrollpaneTag.unwrap();
+		scrollpaneTag.css({
+			'position': 'relative',
+			'max-width': 'none'
+		});
+		scrollpaneActive = false;
+	}
+	/////////////////////////////////////////////////
+	toggle_scrollpane = function() {
+		if (scrollPaneTab.hasClass('open')) {
+			scrollpaneWrapper.stop().animate({ right: -320}, 400);
+		} else {
+			scrollpaneWrapper.stop().animate({ right: 0 }, 400);
+		}
+		scrollPaneTab.toggleClass("open");
+		return false;
+	}
+	/////////////////////////////////////////////////
+	init_scrollpane = function() {
+		scrollpaneApi = scrollpaneTag.wrap("<div id='scrollpane-wrapper' />").jScrollPane({
+			showArrows: true,
+			autoReinitialise: false
+		}).data('jsp');	
+		scrollpaneWrapper = $("#scrollpane-wrapper");
+		$("<a href='#' id='scrollpane-tab' class='open'><span></span></a>").prependTo(scrollpaneWrapper);
+		scrollPaneTab = $("#scrollpane-tab");
+		scrollPaneTab.on('click', toggle_scrollpane);
+		scrollpaneActive = true;
+	}
 
 	/////////////////////////////////////////////////
-  init_scrollpane = function() {
 	
-		scrollpaneTag.wrap("<div id='scrollpane-wrapper'></div>");
-		$("<a href='#' id='scrollpane-tab' class='open'><span></span></a>").prependTo($("#scrollpane-wrapper"))
-		.bind('click',
-			function(e) {
-				var el = $(this);
-				if (el.hasClass('open')) {
-					$("#scrollpane-wrapper").stop().animate({ right: -320}, 400);
-				} else {
-					$("#scrollpane-wrapper").stop().animate({ right: 0 }, 400);
-				}
-				el.toggleClass("open");
-				e.preventDefault();
-	    });;  
+	console.log(screen.width)
 	
-		scrollpaneTag.jScrollPane({
-			showArrows: true,
-			autoReinitialise: true
+	if(screen.width > 820 ) {
+		if(viewport_wh().w > 819 ) {
+			init_scrollpane();
+		}
+		$(window).on("throttledresize", function() {
+			if(viewport_wh().w > 820 && !scrollpaneActive ) {
+				init_scrollpane();
+			} else if(scrollpaneActive) {
+				remove_scrollpane();
+			}
 		});
-		scrollpaneApi = scrollpaneTag.data('jsp');
-  }
-
-	var scrollpaneTag = $('.page_content:first'),
-			scrollpaneApi;
- 
-  if($('body').hasClass('mobile')) {
+	}
+	
+	if($('body').hasClass('mobile')) {
+	
 	}
 	else {
 		if($('.images:first').length > 0) {
 			Gallery.init($('.images:first'));
 		}
-		init_scrollpane();
 	}
+
 	
 });
